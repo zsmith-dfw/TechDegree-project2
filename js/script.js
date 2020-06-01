85b553a439ea5651f7587685b355704aaf1c0fa3
Zach Smith <rysskismith@gmail.com>
2020-06-01 16:33:04 -0500

    added starter files

---
+++ js/script.js
@@ -0,0 +1,69 @@
+
+
+
+/*** 
+   Add your global variables that store the DOM elements you will 
+   need to reference and/or manipulate. 
+   
+   But be mindful of which variables should be global and which 
+   should be locally scoped to one of the two main functions you're 
+   going to create. A good general rule of thumb is if the variable 
+   will only be used inside of a function, then it can be locally 
+   scoped to that function.
+***/
+const student = // this will store the student list item elements in student list
+const item = // this will store the number of items on a page
+
+
+
+/*** 
+   Create the `showPage` function to hide all of the items in the 
+   list except for the ten you want to show.
+
+   Pro Tips: 
+     - Keep in mind that with a list of 54 students, the last page 
+       will only display four.
+     - Remember that the first student has an index of 0.
+     - Remember that a function `parameter` goes in the parens when 
+       you initially define the function, and it acts as a variable 
+       or a placeholder to represent the actual function `argument` 
+       that will be passed into the parens later when you call or 
+       "invoke" the function 
+***/
+
+const showPage = (list, page) => { 
+   if (index of item is >= index of first.child)
+   show item on page && show item <= index of lastChild on page
+}// we want to hide all students except for 10
+// loop over items in the LIST parameter and if it is >= the index of the first
+
+
+
+
+const appendPageLinks = (list) => { // creates and appends funcitoning pagination links
+   total items / max number per page = total pages 
+   create.element = div and set className to pagination 
+   append to div 
+   for (every page add links to other pages)
+   use event.target to go to different pages when they are clicked
+
+   // below is an example from the practice session showing how to embed for loop and conditional in function, will probably need to be tailored 
+   
+// const sectionSelection = (list, section) => {
+//    const startIndex = (section * perPage) - perPage;
+//    const endIndex = (section * perPage) - 1;
+//      for (let i = 0; i < list.length; i ++) {
+//        list[i].style.color = 'whitesmoke';
+//      }
+//      for (let i = 0; i < list.length; i ++) {
+//        if (i >= startIndex && i <= endIndex) {
+//          list[i].style.color = "green";
+//        }
+     }
+ }
+}
+
+
+
+
+
